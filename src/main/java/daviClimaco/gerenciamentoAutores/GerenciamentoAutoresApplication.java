f875a114b75e7f78d2e10492e432d6256823fad1
daviClimaco <davibomfimclimaco@gmail.com>
2026-03-22 22:08:22 -0300

    iniciando pasta repositorio git

---
+++ src/main/java/daviClimaco/gerenciamentoAutores/GerenciamentoAutoresApplication.java
@@ -0,0 +1,13 @@
+package daviClimaco.gerenciamentoAutores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAutoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAutoresApplication.class, args);
+	}
+
+}
